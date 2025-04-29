238c20c2feec34bd41c9cb77567ec804b0e97572
Sharma-Pragya <shpragya8@gmail.com>
2025-04-29 15:28:51 -0700

    vqa files uploaded

---
+++ vqa/updated/config.py
@@ -0,0 +1,8 @@
+models_directory = "models/"
+dataset_directory = "dataset/"
+hf_token_path = "../../hf-token.txt"
+
+model_name="google/gemma-3-1b-it"
+# model_name="vikhyatk/moondream2"
+dataset=dataset_directory+"val2014"
+dataset_json=dataset_directory+"val.json"
